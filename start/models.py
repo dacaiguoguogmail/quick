03e1ea9156978c9cd7652057810dccb04fddf11a
sunyanguo <sunyanguo@lvmama.com>
2018-03-02 20:17:54 +0800

    添加接口

--- start/models.py
+++ start/models.py
@@ -22,3 +22,53 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+
+class UserModel2(models.Model):
+    def __str__(self):
+        return self.dt
+    lo = models.CharField(max_length=200)
+    prov = models.CharField(max_length=200)
+    dt = models.CharField(max_length=200)
+    tec = models.CharField(max_length=200)
+    ct = models.CharField(max_length=200)
+    ui = models.CharField(max_length=200)
+    ns = models.CharField(max_length=200)
+    ver = models.CharField(max_length=200)
+    ot = models.CharField(max_length=200)
+    mn = models.CharField(max_length=200)
+
+
+class UserDataModel2(models.Model):
+    def __str__(self):
+        return self.av
+    
+    usermodel = models.ForeignKey(UserModel2, on_delete=models.CASCADE)
+    av = models.CharField(max_length=200)
+    an = models.CharField(max_length=200)
+    ec = models.CharField(max_length=200)
+    rsd = models.CharField(max_length=200)
+    scsd = models.CharField(max_length=200)
+    ecd = models.CharField(max_length=200)
+    nt = models.CharField(max_length=200)
+    rft = models.CharField(max_length=200)
+    i10 = models.CharField(max_length=200)
+    sced = models.CharField(max_length=200)
+    iuf = models.CharField(max_length=200)
+    red = models.CharField(max_length=200)
+    fsd = models.CharField(max_length=200)
+    spsd = models.CharField(max_length=200)
+    dlsd = models.CharField(max_length=200)
+    sped = models.CharField(max_length=200)
+    dz = models.CharField(max_length=200)
+    pi = models.CharField(max_length=200)
+    rt = models.CharField(max_length=200)
+    rc = models.CharField(max_length=200)
+    dled = models.CharField(max_length=200)
+    csd = models.CharField(max_length=200)
+    re = models.CharField(max_length=200)
+    at = models.CharField(max_length=200)
+    npn = models.CharField(max_length=200)
+    pc = models.CharField(max_length=200)
+    ced = models.CharField(max_length=200)
+    cp = models.CharField(max_length=200)
--- start/views.py
+++ start/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.utils import timezone 
 from django.views import generic
-
-from .models import Choice, Question
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Choice, Question, UserDataModel2, UserModel2
+import json
+import gzip
+import base64
 
 class IndexView(generic.ListView):
     template_name = 'start/index.html'
@@ -43,4 +47,61 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse('start:results', args=(question.id,)))
 
 #    return HttpResponse("You're voting on question %s." % question_id)
+@api_view(['POST', 'HEAD'])
+def addapi(request):
+    orgss = request.data['data']
+    base64.b64decode(orgss)
+    orgssbytes = base64.b64decode(orgss)
+    orgssjson = gzip.decompress(orgssbytes)
+    dataDict = json.loads(orgssjson)
+    ause = UserModel2()
+    ause.lo = dataDict['lo']
+    ause.prov = dataDict['prov']
+    ause.dt = dataDict['dt']
+    ause.tec = dataDict['tec']
+    ause.ct = dataDict['ct']
+    ause.ui = dataDict['ui']
+    ause.ns = dataDict['ns']
+    ause.ver = dataDict['ver']
+    ause.ot = dataDict['ot']
+    ause.mn = dataDict['mn']
+    ause.save()
+    result = {}
+    for k in dataDict['data']:
+        # auserdata = UserDataModel2()
+        # auserdata.av = 'av'
+        # auserdata.an = 'an'
+        # auserdata.ec = 'ec'
+        # auserdata.rsd = 'rsd'
+        # auserdata.scsd = 'scsd'
+        # auserdata.ecd = 'ecd'
+        # auserdata.nt = 'nt'
+        # auserdata.rft = 'rft'
+        # auserdata.i10 = 'i10'
+        # auserdata.sced = 'sced'
+        # auserdata.iuf = 'iuf'
+        # auserdata.red = 'red'
+        # auserdata.fsd = 'fsd'
+        # auserdata.spsd = 'spsd'
+        # auserdata.dlsd = 'dlsd'
+        # auserdata.sped = 'sped'
+        # auserdata.dz = 'dz'
+        # auserdata.pi = 'pi'
+        # auserdata.rt = 'rt'
+        # auserdata.rc = 'rc'
+        # auserdata.dled = 'dled'
+        # auserdata.csd = 'csd'
+        # auserdata.re = 're'
+        # auserdata.at = 'at'
+        # auserdata.npn = 'npn'
+        # auserdata.pc = 'pc'
+        # auserdata.ced = 'ced'
+        # auserdata.cp = 'cp'
+        # auserdata2 = UserDataModel2.objects.get(pk=1)
+        ause2 = UserModel2.objects.get(pk=1)
+        ause2.userdatamodel2_set.create(av = 'av',an = 'an',ec = 'ec',rsd = 'rsd',scsd = 'scsd',ecd = 'ecd',nt = 'nt',rft = 'rft',i10 = 'i10',sced = 'sced',iuf = 'iuf',red = 'red',fsd = 'fsd',spsd = 'spsd',dlsd = 'dlsd',sped = 'sped',dz = 'dz',pi = 'pi',rt = 'rt',rc = 'rc',dled = 'dled',csd = 'csd',re = 're',at = 'at',npn = 'npn',pc = 'pc',ced = 'ced',cp = 'cp')
+        # auserdata.save()
+        ause2.save()
+    
+    return Response(json.dumps(result), content_type='application/json')
 
